315d1b32bda8169cacd80cc5922fa3e59da53b0f
jupiterbjy <jupiterbjy@gmail.com>
2021-08-03 01:09:58 +0900

    Recommitting lost files

---
+++ TwitchStreamNotifyBot/PushMethod/twitter_push.py
@@ -0,0 +1,45 @@
+from typing import Union
+
+import tweepy
+from loguru import logger
+
+from .base import Push
+
+
+class TwitterPush(Push):
+    def __init__(self, config: dict):
+        self.config = config["twitter"]
+
+        self.content: str = self.config["content"]
+
+        self.api_key = self.config["api key"]
+        self.api_secret = self.config["api secret key"]
+
+        self.token = self.config["access token"]
+        self.token_secret = self.config["access token secret"]
+
+        self.api: Union[None, tweepy.API] = None
+
+        self.auth()
+
+    def auth(self):
+
+        if not all((self.api_key, self.api_secret, self.token, self.token_secret)):
+            logger.info("One or more Twitter parameters empty, skipping.")
+            raise ValueError()
+
+        logger.info("Twitch auth started.")
+
+        auth = tweepy.OAuthHandler(self.api_key, self.api_secret)
+
+        auth.set_access_token(self.token, self.token_secret)
+
+        self.api = tweepy.API(auth)
+
+        logger.info("Twitch auth complete.")
+
+    def send(self, content):
+
+        self.api.update_status(self.content.format(content))
+
+        logger.info("Notified to twitter.")
